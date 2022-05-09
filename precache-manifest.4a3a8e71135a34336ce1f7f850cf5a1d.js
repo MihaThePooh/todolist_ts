fa2d314f815751943c1cfd7d9dda7bbfef5134d6
MihaThePooh <63107837+MihaThePooh@users.noreply.github.com>
2022-05-09 19:21:03 +0300

    Updates

---
+++ precache-manifest.4a3a8e71135a34336ce1f7f850cf5a1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb6d0ccecbe4f525a3a794f2b15bffc",
+    "url": "/todolist_ts/index.html"
+  },
+  {
+    "revision": "ea4579376d6bd40219c5",
+    "url": "/todolist_ts/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "cc004893087da7d8b09f",
+    "url": "/todolist_ts/static/js/2.5088fb72.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist_ts/static/js/2.5088fb72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea4579376d6bd40219c5",
+    "url": "/todolist_ts/static/js/main.0690ef22.chunk.js"
+  },
+  {
+    "revision": "cfe20a8a4f4c50f167dd",
+    "url": "/todolist_ts/static/js/runtime-main.83004bf9.js"
+  }
+]);
